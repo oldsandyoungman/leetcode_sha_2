9f4621276ea7a9c0298513ad3bc61d724ca2189a
oldsandyoungman <874997364@qq.com>
2021-11-18 17:12:07 +0800

    初始化

---
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/LeetcodeSha2Application.java
@@ -0,0 +1,13 @@
+package com.example.leetcode_sha_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeSha2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeSha2Application.class, args);
+    }
+
+}
---
+++ leetcode_sha_2/src/test/java/com/example/leetcode_sha_2/LeetcodeSha2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcode_sha_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeSha2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
