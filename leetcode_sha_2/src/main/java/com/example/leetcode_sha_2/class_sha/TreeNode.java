c849ae5b0896a25bf68006968c76f4e763ae095e
oldsandyoungman <874997364@qq.com>
2021-11-22 15:12:40 +0800

    添加TreeNode大类

---
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/class_sha/TreeNode.java
@@ -0,0 +1,14 @@
+package com.example.leetcode_sha_2.class_sha;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
