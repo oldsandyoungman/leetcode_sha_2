9068eedd1eda2549ec154da43bae8023064fce92
oldsandyoungman <874997364@qq.com>
2021-11-29 10:24:41 +0800

    二叉树翻转，子树问题

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/jian_zhi_offer/s26_no.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/jian_zhi_offer/s26_no.java
@@ -5,6 +5,19 @@ import com.example.leetcode_sha_2.class_sha.TreeNode;
 public class s26_no {
 
     public static void main(String[] args) {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        TreeNode e = new TreeNode(3);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+
+        System.out.println(isSubStructure(a, e));
+        System.out.println(isSubStructure2(a, e));
+        System.out.println(1e9+7 - 1e9);
 
     }
 
@@ -12,13 +25,30 @@ public class s26_no {
 //
 //    B是A的子结构， 即 A中有出现和B相同的结构和节点值。
 
-    public boolean isSubStructure(TreeNode A, TreeNode B) {
+    public static boolean isSubStructure(TreeNode A, TreeNode B) {
         return (A != null && B != null) && (recur(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B));
     }
-    boolean recur(TreeNode A, TreeNode B) {
+    public static boolean recur(TreeNode A, TreeNode B) {
         if(B == null) return true;
         if(A == null || A.val != B.val) return false;
         return recur(A.left, B.left) && recur(A.right, B.right);
+    }
+
+    public static boolean isSubStructure2(TreeNode A, TreeNode B) {
+        if(A==null || B==null){
+            return false;
+        }
+        return traverse(A, B) && isSubStructure2(A.left, B) && isSubStructure2(A.right, B);
+    }
+    //A和B是否完全一致
+    public static boolean traverse(TreeNode A, TreeNode B){
+        if(B==null){
+            return true;
+        }
+        if(A==null || A.val!=B.val){
+            return false;
+        }
+        return traverse(A.left, B.left) && traverse(A.right, B.right);
     }
 
 }
