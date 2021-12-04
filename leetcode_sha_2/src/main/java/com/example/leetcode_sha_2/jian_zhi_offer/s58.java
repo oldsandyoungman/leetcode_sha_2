9dce3b2218188350add94ae72e7984f5698bedd2
oldsandyoungman <874997364@qq.com>
2021-12-04 09:19:45 +0800

    改个名字

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/jian_zhi_offer/s58.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/jian_zhi_offer/s58.java
@@ -1,21 +1,40 @@
 package com.example.leetcode_sha_2.jian_zhi_offer;
 
+import java.util.Arrays;
+
 public class s58 {
 
     public static void main(String[] args) {
-
+        String s = "  hello world!  ";
+        System.out.println(Arrays.toString(s.trim().split(" ")));
     }
 
-//    字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+//    输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
 //
 //    来源：力扣（LeetCode）
-//    链接：https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+//    链接：https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof
 //    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-    public static String reverseLeftWords(String s, int n) {
-        String s1 = s.substring(0, n);
-        String s2 = s.substring(n);
-        return s2+s1;
+    public static String reverseWords(String s) {
+        s = s.trim();
+        int n = s.length();
+        int left = n-1;
+        int right = n-1;
+        StringBuilder sb = new StringBuilder();
+
+        while(left>=0){
+            while(left>=0 && s.charAt(left)!=' '){
+                left--;
+            }
+            sb.append(s, left+1, right+1).append(" ");
+            while(left>=0 && s.charAt(left)==' '){
+                left--;
+            }
+            right = left;
+        }
+
+        return sb.toString().trim();
+
     }
 
 
