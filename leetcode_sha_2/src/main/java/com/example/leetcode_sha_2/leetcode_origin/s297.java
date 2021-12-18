7154f9cf1bb23826e22aa0c37e77d52003ff1af3
oldsandyoungman <874997364@qq.com>
2021-12-18 09:57:16 +0800

    二叉树序列化问题（3种方法）

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s297.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s297.java
@@ -3,6 +3,8 @@ package com.example.leetcode_sha_2.leetcode_origin;
 import com.example.leetcode_sha_2.class_sha.TreeNode;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class s297 {
 
@@ -20,7 +22,13 @@ public class s297 {
 
         String tmp = serialize(a);
         System.out.println(tmp);
-        System.out.println(deserialize(tmp));
+        TreeNode res = deserialize(tmp);
+        System.out.println(res);
+
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.addLast(1);
+        q.clear();
+        System.out.println();
 
     }
 
@@ -40,42 +48,179 @@ public class s297 {
     static String NULL = "#";
     static StringBuilder sb;
 
+
+
+////前序
+//    // Encodes a tree to a single string.
+//    public static String serialize(TreeNode root) {
+//        sb = new StringBuilder();
+//        traverse(root);
+//        return sb.toString();
+//    }
+//
+//    public static void traverse(TreeNode root){
+//        if(root==null){
+//            sb.append(NULL).append(SEP);
+//            return;
+//        }
+//        sb.append(root.val).append(SEP);
+//        traverse(root.left);
+//        traverse(root.right);
+//    }
+//
+//    // Decodes your encoded data to tree.
+//    public static TreeNode deserialize(String data) {
+//        String[] s = data.split(SEP);
+//        ArrayDeque<String> q = new ArrayDeque<>();
+//        for(String cur : s){
+//            q.addLast(cur);
+//        }
+//        return traverse2(q);
+//    }
+//
+//    public static TreeNode traverse2(ArrayDeque<String> q){
+//        if(NULL.equals(q.getFirst())){
+//            q.removeFirst();
+//            return null;
+//        }
+//        TreeNode root = new TreeNode(Integer.parseInt(q.removeFirst()));
+//        root.left = traverse2(q);
+//        root.right = traverse2(q);
+//        return root;
+//    }
+
+
+
+//后序
+//    // Encodes a tree to a single string.
+//    public static String serialize(TreeNode root) {
+//        sb = new StringBuilder();
+//        traverse(root);
+//        return sb.toString();
+//    }
+//
+//    public static void traverse(TreeNode root){
+//        if (root==null) {
+//            sb.append(NULL).append(SEP);
+//            return;
+//        }
+//        traverse(root.left);
+//        traverse(root.right);
+//        sb.append(root.val).append(SEP);
+//    }
+//
+//    // Decodes your encoded data to tree.
+//    public static TreeNode deserialize(String data) {
+//        String[] ss = data.split(SEP);
+//        ArrayDeque<String> q = new ArrayDeque<>();
+//        for (String s : ss) {
+//            q.addLast(s);
+//        }
+//        return traverse2(q);
+//    }
+//
+//    public static TreeNode traverse2(ArrayDeque<String> q){
+//        if (q.getLast().equals(NULL)) {
+//            q.removeLast();
+//            return null;
+//        }
+//        TreeNode root = new TreeNode(Integer.parseInt(q.removeLast()));
+//        root.right = traverse2(q);
+//        root.left = traverse2(q);
+//        return root;
+//    }
+
+
+//BFS
     // Encodes a tree to a single string.
     public static String serialize(TreeNode root) {
-        sb = new StringBuilder();
-        traverse(root);
+        if (root==null) {
+            return "";
+        }
+        // ArrayDeque 不能加入null，LinkedList可以
+        LinkedList<TreeNode> q = new LinkedList<>();
+        q.addLast(root);
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!q.isEmpty()){
+
+            int n = q.size();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode cur = q.removeFirst();
+                if (cur==null) {
+                    sb.append(NULL).append(SEP);
+                    continue;
+                }
+                sb.append(cur.val).append(SEP);
+                q.addLast(cur.left);
+                q.addLast(cur.right);
+
+            }
+
+        }
+
         return sb.toString();
+
+//        if (root == null) return "";
+//        StringBuilder sb = new StringBuilder();
+//        // 初始化队列，将 root 加入队列
+//        Queue<TreeNode> q = new LinkedList<>();
+//        q.offer(root);
+//
+//        while (!q.isEmpty()) {
+//            TreeNode cur = q.poll();
+//
+//            /* 层级遍历代码位置 */
+//            if (cur == null) {
+//                sb.append(NULL).append(SEP);
+//                continue;
+//            }
+//            sb.append(cur.val).append(SEP);
+//            /*****************/
+//
+//            q.offer(cur.left);
+//            q.offer(cur.right);
+//        }
+//
+//        return sb.toString();
+
     }
 
-    public static void traverse(TreeNode root){
-        if(root==null){
-            sb.append(NULL).append(SEP);
-            return;
-        }
-        sb.append(root.val).append(SEP);
-        traverse(root.left);
-        traverse(root.right);
-    }
 
     // Decodes your encoded data to tree.
     public static TreeNode deserialize(String data) {
-        String[] s = data.split(SEP);
-        ArrayDeque<String> q = new ArrayDeque<>();
-        for(String cur : s){
-            q.addLast(cur);
+        String[] ss = data.split(SEP);
+
+
+        ArrayDeque<TreeNode> q = new ArrayDeque<>();
+        TreeNode root = new TreeNode(Integer.parseInt(ss[0]));
+        q.add(root);
+
+        int n = ss.length;
+        int i = 1;
+
+        while (i<n) {
+
+            TreeNode cur = q.removeFirst();
+            if (!ss[i].equals(NULL)) {
+                cur.left = new TreeNode(Integer.parseInt(ss[i]));
+                q.addLast(cur.left);
+            }
+            i++;
+            if (!ss[i].equals(NULL)) {
+                cur.right = new TreeNode(Integer.parseInt(ss[i]));
+                q.addLast(cur.right);
+            }
+            i++;
+
         }
-        return traverse2(q);
+
+        return root;
+
+
     }
 
-    public static TreeNode traverse2(ArrayDeque<String> q){
-        if(NULL.equals(q.getFirst())){
-            q.removeFirst();
-            return null;
-        }
-        TreeNode root = new TreeNode(Integer.parseInt(q.removeFirst()));
-        root.left = traverse2(q);
-        root.right = traverse2(q);
-        return root;
-    }
 
 }
