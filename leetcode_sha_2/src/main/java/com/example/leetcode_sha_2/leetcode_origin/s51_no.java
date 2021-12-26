1c58a44b66d07c44e354d399c04b79d0135a7093
oldsandyoungman <874997364@qq.com>
2021-12-26 18:33:42 +0800

    N皇后问题，加了剪枝的快速方法

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s51_no.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s51_no.java
@@ -22,19 +22,89 @@ public class s51_no {
 //    链接：https://leetcode-cn.com/problems/n-queens
 //    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-    public static List<List<String>> solveNQueens(int n) {
+//    public static List<List<String>> solveNQueens(int n) {
+//        res = new ArrayList<>();
+//        List<String> panel = new ArrayList<>();
+//        backtrack(panel, 0, n);
+//        return res;
+//    }
+//    public static List<List<String>> res;
+//    // Q: 有棋子
+//    // .: 没棋子
+//    public static void backtrack(List<String> panel, int k, int n){
+//        if (k==n){
+//            res.add(new ArrayList<>(panel));
+//            return;
+//        }
+//
+//        for (int i = 0; i < n; i++) {
+//
+//            if (isValid(panel, k, i, n)){
+//
+//                StringBuilder sb = new StringBuilder();
+//                sb.append(".".repeat(i));
+//                sb.append('Q');
+//                sb.append(".".repeat(Math.max(0, n - (i + 1))));
+//
+//                panel.add(sb.toString());
+//
+//                backtrack(panel, k+1, n);
+//
+//                panel.remove(panel.size()-1);
+//            }
+//
+//        }
+//
+//    }
+//
+//    private static boolean isValid(List<String> panel, int k, int i, int n) {
+//        for (int j = 0; j < k; j++) {
+//            char cur = panel.get(j).charAt(i);
+//            if (cur=='Q'){
+//                return false;
+//            }
+//        }
+//        int cur_l_col = i-1;
+//        int cur_l_row = k-1;
+//        while (cur_l_col>=0 && cur_l_row>=0){
+//            char cur = panel.get(cur_l_row).charAt(cur_l_col);
+//            if (cur=='Q'){
+//                return false;
+//            }
+//            cur_l_row--;
+//            cur_l_col--;
+//        }
+//
+//        int cur_r_col = i+1;
+//        int cur_r_row = k-1;
+//        while (cur_r_col<n && cur_r_row>=0){
+//            char cur = panel.get(cur_r_row).charAt(cur_r_col);
+//            if (cur=='Q'){
+//                return false;
+//            }
+//            cur_r_row--;
+//            cur_r_col++;
+//        }
+//
+//        return true;
+//
+//    }
+
+
+//  只判断有没有的剪枝方法
+    public static boolean solveNQueens(int n) {
         res = new ArrayList<>();
         List<String> panel = new ArrayList<>();
-        backtrack(panel, 0, n);
-        return res;
+
+        return backtrack(panel, 0, n);
     }
     public static List<List<String>> res;
     // Q: 有棋子
     // .: 没棋子
-    public static void backtrack(List<String> panel, int k, int n){
+    public static boolean backtrack(List<String> panel, int k, int n){
         if (k==n){
             res.add(new ArrayList<>(panel));
-            return;
+            return true;
         }
 
         for (int i = 0; i < n; i++) {
@@ -48,12 +118,18 @@ public class s51_no {
 
                 panel.add(sb.toString());
 
-                backtrack(panel, k+1, n);
+                boolean tmp = backtrack(panel, k+1, n);
+
+                if (tmp) {
+                    return true;
+                }
 
                 panel.remove(panel.size()-1);
             }
 
         }
+
+        return false;
 
     }
 
