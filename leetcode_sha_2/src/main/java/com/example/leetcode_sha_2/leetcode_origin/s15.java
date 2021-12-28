3de51e53d56ae721eae3f676cfff089e79a7cd76
oldsandyoungman <874997364@qq.com>
2021-12-28 14:24:29 +0800

    nSum问题重做

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s15.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s15.java
@@ -2,14 +2,15 @@ package com.example.leetcode_sha_2.leetcode_origin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class s15 {
 
     public static void main(String[] args) {
-//        int[] nums = {-1,0,1,2,-1,-4};
+        int[] nums = {-1,0,1,2,-1,-4};
 //        int[] nums = {0,0,0};
-        int[] nums = {1,1,1};
+//        int[] nums = {1,1,1};
         System.out.println(threeSum(nums));
     }
 
@@ -21,78 +22,139 @@ public class s15 {
 //    链接：https://leetcode-cn.com/problems/3sum
 //    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+
+
     public static List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
-
-        List<List<Integer>> result = new ArrayList<>();
-
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
         int i = 0;
-
         while(i<n){
-
-            int cur_num = nums[i];
-            int target = -cur_num;
-            List<List<Integer>> res = twoSum(nums, i+1, target);
-
-            for(List<Integer> tmp : res){
-                tmp.add(cur_num);
-                result.add(tmp);
+            int target = -nums[i];
+            List<List<Integer>> tmp = twoSum(nums, target, i+1);
+            for(List<Integer> cur : tmp){
+                cur.add(nums[i]);
+                res.add(new LinkedList<>(cur));
             }
-
             i++;
-
-            while(i<n && nums[i]==cur_num){
+            while(i<n && nums[i]==-target){
                 i++;
             }
-
         }
-
-        return result;
-
+        return res;
     }
-    public static List<List<Integer>> twoSum(int[] nums, int start, int target){
+
+    public static List<List<Integer>> twoSum(int[] nums, int target, int start){
         int n = nums.length;
         int left = start;
         int right = n-1;
-
-        List<List<Integer>> res = new ArrayList<>();
-
-        while(left<right){
-            int left_num = nums[left];
-            int right_num = nums[right];
-            int sum = left_num + right_num;
-            if(sum<target){
-                left++;
-                while(left<right && nums[left]==left_num){
-                    left++;
-                }
-            }else if(sum>target){
+        List<List<Integer>> res = new LinkedList<>();
+        while(left<=right){
+            int ln = nums[left];
+            int rn = nums[right];
+            int s = ln + rn;
+            if(s>target){
                 right--;
-                while(left<right && nums[right]==right_num){
+                while(right>left && nums[right]==rn){
                     right--;
+                }
+            }else if(s<target){
+                left++;
+                while(right>left && nums[left]==ln){
+                    left++;
                 }
             }else{
-                List<Integer> tmp = new ArrayList<>();
-                tmp.add(left_num);
-                tmp.add(right_num);
+                List<Integer> tmp = new LinkedList<>();
+                tmp.add(ln);
+                tmp.add(rn);
                 res.add(tmp);
-
                 left++;
-                while(left<right && nums[left]==left_num){
+                right--;
+                while(right>left && nums[left]==ln){
                     left++;
                 }
-
-                right--;
-                while(left<right && nums[right]==right_num){
+                while(right>left && nums[right]==rn){
                     right--;
                 }
-
             }
         }
-
         return res;
-
     }
+
+
+
+//    public static List<List<Integer>> threeSum(int[] nums) {
+//        Arrays.sort(nums);
+//        int n = nums.length;
+//
+//        List<List<Integer>> result = new ArrayList<>();
+//
+//        int i = 0;
+//
+//        while(i<n){
+//
+//            int cur_num = nums[i];
+//            int target = -cur_num;
+//            List<List<Integer>> res = twoSum(nums, i+1, target);
+//
+//            for(List<Integer> tmp : res){
+//                tmp.add(cur_num);
+//                result.add(tmp);
+//            }
+//
+//            i++;
+//
+//            while(i<n && nums[i]==cur_num){
+//                i++;
+//            }
+//
+//        }
+//
+//        return result;
+//
+//    }
+//    public static List<List<Integer>> twoSum(int[] nums, int start, int target){
+//        int n = nums.length;
+//        int left = start;
+//        int right = n-1;
+//
+//        List<List<Integer>> res = new ArrayList<>();
+//
+//        while(left<right){
+//            int left_num = nums[left];
+//            int right_num = nums[right];
+//            int sum = left_num + right_num;
+//            if(sum<target){
+//                left++;
+//                while(left<right && nums[left]==left_num){
+//                    left++;
+//                }
+//            }else if(sum>target){
+//                right--;
+//                while(left<right && nums[right]==right_num){
+//                    right--;
+//                }
+//            }else{
+//                List<Integer> tmp = new ArrayList<>();
+//                tmp.add(left_num);
+//                tmp.add(right_num);
+//                res.add(tmp);
+//
+//                left++;
+//                while(left<right && nums[left]==left_num){
+//                    left++;
+//                }
+//
+//                right--;
+//                while(left<right && nums[right]==right_num){
+//                    right--;
+//                }
+//
+//            }
+//        }
+//
+//        return res;
+//
+//    }
 
 }
--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s18.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s18.java
@@ -25,78 +25,141 @@ public class s18 {
 //    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 
+
     public static List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         return nSum(nums, target, 4, 0);
     }
-    public static List<List<Integer>> nSum(int[] nums, int target, int N, int start) {
+    public static List<List<Integer>> nSum(int[] nums, int target, int N, int start){
         int n = nums.length;
-
         List<List<Integer>> res = new ArrayList<>();
-
         if(N==2){
             int left = start;
             int right = n-1;
             while(left<right){
-                int left_num = nums[left];
-                int right_num = nums[right];
-                int sum = left_num + right_num;
-
-                if(sum>target){
+                int ln = nums[left];
+                int rn = nums[right];
+                int s = ln+rn;
+                if(s>target){
                     right--;
-                    while(left<right && nums[right]==right_num){
+                    while(left<right && nums[right]==rn){
                         right--;
                     }
-                }else if(sum<target){
+                }else if(s<target){
                     left++;
-                    while(left<right && nums[left]==left_num){
+                    while(left<right && nums[left]==ln){
                         left++;
                     }
                 }else{
                     List<Integer> tmp = new ArrayList<>();
-                    tmp.add(left_num);
-                    tmp.add(right_num);
+                    tmp.add(ln);
+                    tmp.add(rn);
                     res.add(tmp);
-
                     right--;
-                    while(left<right && nums[right]==right_num){
+                    while(left<right && nums[right]==rn){
                         right--;
                     }
-
                     left++;
-                    while(left<right && nums[left]==left_num){
+                    while(left<right && nums[left]==ln){
                         left++;
                     }
-
                 }
-
             }
-
             return res;
 
         }else{
 
             int i = start;
-
             while(i<n){
-                int cur = nums[i];
-                List<List<Integer>> cur_res = nSum(nums, target-cur, N-1, i+1);
-                for(List<Integer> tmp : cur_res){
-                    tmp.add(cur);
-                    res.add(tmp);
+                int tt = target - nums[i];
+                List<List<Integer>> tmp = nSum(nums, tt, N-1, i+1);
+                for(List<Integer> cur : tmp){
+                    cur.add(nums[i]);
+                    // res.add(cur);
+                    res.add(new ArrayList<>(cur));
                 }
-
                 i++;
-
-                while(i<n && nums[i]==cur){
+                while(i<n && nums[i]==target-tt){
                     i++;
                 }
-
             }
-
             return res;
 
         }
     }
+
+//    public static List<List<Integer>> fourSum(int[] nums, int target) {
+//        Arrays.sort(nums);
+//        return nSum(nums, target, 4, 0);
+//    }
+//    public static List<List<Integer>> nSum(int[] nums, int target, int N, int start) {
+//        int n = nums.length;
+//
+//        List<List<Integer>> res = new ArrayList<>();
+//
+//        if(N==2){
+//            int left = start;
+//            int right = n-1;
+//            while(left<right){
+//                int left_num = nums[left];
+//                int right_num = nums[right];
+//                int sum = left_num + right_num;
+//
+//                if(sum>target){
+//                    right--;
+//                    while(left<right && nums[right]==right_num){
+//                        right--;
+//                    }
+//                }else if(sum<target){
+//                    left++;
+//                    while(left<right && nums[left]==left_num){
+//                        left++;
+//                    }
+//                }else{
+//                    List<Integer> tmp = new ArrayList<>();
+//                    tmp.add(left_num);
+//                    tmp.add(right_num);
+//                    res.add(tmp);
+//
+//                    right--;
+//                    while(left<right && nums[right]==right_num){
+//                        right--;
+//                    }
+//
+//                    left++;
+//                    while(left<right && nums[left]==left_num){
+//                        left++;
+//                    }
+//
+//                }
+//
+//            }
+//
+//            return res;
+//
+//        }else{
+//
+//            int i = start;
+//
+//            while(i<n){
+//                int cur = nums[i];
+//                List<List<Integer>> cur_res = nSum(nums, target-cur, N-1, i+1);
+//                for(List<Integer> tmp : cur_res){
+//                    tmp.add(cur);
+//                    res.add(tmp);
+//                }
+//
+//                i++;
+//
+//                while(i<n && nums[i]==cur){
+//                    i++;
+//                }
+//
+//            }
+//
+//            return res;
+//
+//        }
+//    }
 
 }
