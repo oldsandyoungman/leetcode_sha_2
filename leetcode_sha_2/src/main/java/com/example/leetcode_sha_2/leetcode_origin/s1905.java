5b87f0ed7998926ec5422102182979dd502535b7
oldsandyoungman <874997364@qq.com>
2021-12-28 11:23:59 +0800

    岛屿的包含问题重做

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s1905.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s1905.java
@@ -33,51 +33,100 @@ public class s1905 {
 //    链接：https://leetcode-cn.com/problems/count-sub-islands
 //    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
+
+
+
+
+
     public static int countSubIslands(int[][] grid1, int[][] grid2) {
         int m = grid1.length;
         int n = grid1[0].length;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid1[i][j]==0 && grid2[i][j]==1){
-                    dfs(grid2, i, j);
-                }
-            }
-        }
-
         int res = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid2[i][j] == 1){
-                    res++;
-                    dfs(grid2, i, j);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid2[i][j]==1){
+                    boolean flag = dfs(grid1, grid2, i, j);
+                    if(flag){
+                        res++;
+                    }
                 }
             }
         }
-
         return res;
+    }
+    public static boolean dfs(int[][] grid1, int[][] grid2, int i, int j){
+        int m = grid1.length;
+        int n = grid1[0].length;
+        if(i<0 || i>=m || j<0 || j>=n){
+            return true;
+        }
+        if(grid2[i][j]==0){
+            return true;
+        }
 
+        grid2[i][j] = 0;
+        if(grid1[i][j]==0){
+            return false;
+        }
+
+        boolean f1 = dfs(grid1, grid2, i-1, j);
+        boolean f2 = dfs(grid1, grid2, i+1, j);
+        boolean f3 = dfs(grid1, grid2, i, j-1);
+        boolean f4 = dfs(grid1, grid2, i, j+1);
+        return f1 && f2 && f3 && f4;
 
     }
 
-    public static void dfs(int[][] grid, int i, int j){
-        int m = grid.length;
-        int n = grid[0].length;
 
-        if (i<0 || j<0 || i>=m || j>=n){
-            return;
-        }
-        if (grid[i][j]==0){
-            return;
-        }
-        grid[i][j] = 0;
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
 
-    }
+
+
+
+//    public static int countSubIslands(int[][] grid1, int[][] grid2) {
+//        int m = grid1.length;
+//        int n = grid1[0].length;
+//
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (grid1[i][j]==0 && grid2[i][j]==1){
+//                    dfs(grid2, i, j);
+//                }
+//            }
+//        }
+//
+//        int res = 0;
+//
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (grid2[i][j] == 1){
+//                    res++;
+//                    dfs(grid2, i, j);
+//                }
+//            }
+//        }
+//
+//        return res;
+//
+//
+//    }
+//
+//    public static void dfs(int[][] grid, int i, int j){
+//        int m = grid.length;
+//        int n = grid[0].length;
+//
+//        if (i<0 || j<0 || i>=m || j>=n){
+//            return;
+//        }
+//        if (grid[i][j]==0){
+//            return;
+//        }
+//        grid[i][j] = 0;
+//        dfs(grid, i+1, j);
+//        dfs(grid, i-1, j);
+//        dfs(grid, i, j+1);
+//        dfs(grid, i, j-1);
+//
+//    }
 
 
 
