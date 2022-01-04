7dd63c5c007b95fe9bf4aaaadef663466b7816ad
oldsandyoungman <874997364@qq.com>
2022-01-04 16:01:26 +0800

    重复子树问题重做

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s652.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s652.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class s652 {
 
     public static void main(String[] args) {
+        TreeNode root = TreeNode.create_by_matrix(new Integer[]{0, 0, 0, 0, null, null, 0, null, null, null, 0});
+        List<TreeNode> tt = findDuplicateSubtrees(root);
+        System.out.println(Integer.parseInt("A",16));
 
     }
 
@@ -16,31 +19,55 @@ public class s652 {
 //
 //    两棵树重复是指它们具有相同的结构以及相同的结点值。
 
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+//    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+//        res = new LinkedList<>();
+//        m = new HashMap<>();
+//        traverse(root);
+//        return res;
+//
+//    }
+//    public List<TreeNode> res;
+//    public HashMap<String, Integer> m;
+//    public String traverse(TreeNode root){
+//        if(root==null){
+//            return "#";
+//        }
+//        String left = traverse(root.left);
+//        String right = traverse(root.right);
+//
+//        String ss = left + "," + right + "," + root.val;
+//        int fre = m.getOrDefault(ss, 0);
+//        if(fre==1){
+//            res.add(root);
+//        }
+//        m.put(ss, fre+1);
+//
+//        return ss;
+//
+//    }
+
+
+    public static List<TreeNode> findDuplicateSubtrees(TreeNode root) {
         res = new LinkedList<>();
         m = new HashMap<>();
         traverse(root);
         return res;
-
     }
-    public List<TreeNode> res;
-    public HashMap<String, Integer> m;
-    public String traverse(TreeNode root){
+    public static List<TreeNode> res;
+    public static HashMap<String, Integer> m;
+    public static String traverse(TreeNode root){
         if(root==null){
-            return "#";
+            return "#,";
         }
         String left = traverse(root.left);
         String right = traverse(root.right);
-
-        String ss = left + "," + right + "," + root.val;
-        int fre = m.getOrDefault(ss, 0);
-        if(fre==1){
+        String cur = left + right + root.val + "," ;
+        int judge = m.getOrDefault(cur, 0);
+        if(judge==1){
             res.add(root);
         }
-        m.put(ss, fre+1);
-
-        return ss;
-
+        m.put(cur, judge+1);
+        return cur;
     }
 
 }
