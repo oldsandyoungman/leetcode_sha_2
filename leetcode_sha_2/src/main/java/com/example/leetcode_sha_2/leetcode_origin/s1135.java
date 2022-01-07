b40b051a89f70dc43f735a72ecdc9b50da4a6f45
oldsandyoungman <874997364@qq.com>
2022-01-07 15:28:32 +0800

    城市群连通最短路径

---
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s1135.java
@@ -0,0 +1,21 @@
+package com.example.leetcode_sha_2.leetcode_origin;
+
+public class s1135 {
+
+    public static void main(String[] args) {
+
+    }
+
+//    想象一下你是个城市基建规划者，地图上有Ⅳ座城市，它们按以1到N的次序编号。
+//    给你一些可连接的选项conections ，其中每个选项conections [i] =[ city1, city2, cost]表示将城市city1和城市city2连接所要的成本为cost。(连接是双向的，也就是说城市city1和城市city2相连也同样意味着城市city2和城市city1相连)。
+//    计算连通所有城市最小成本。如果无法连通所有城市，则请你返回-1。
+
+    int minimumCost(int n, int[][] connections) {
+
+        return 0;
+    }
+
+
+
+
+}
