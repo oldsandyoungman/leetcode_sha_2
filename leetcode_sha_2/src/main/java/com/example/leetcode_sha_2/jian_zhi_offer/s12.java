ff9dcec8c656977c1562137785cd58f354745fc5
oldsandyoungman <874997364@qq.com>
2022-01-08 10:59:56 +0800

    重做回溯

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/jian_zhi_offer/s12.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/jian_zhi_offer/s12.java
@@ -3,7 +3,9 @@ package com.example.leetcode_sha_2.jian_zhi_offer;
 public class s12 {
 
     public static void main(String[] args) {
-
+        char[][] board = {{'a'}};
+        String word = "a";
+        System.out.println(exist(board, word));
     }
 
 //    给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
@@ -15,39 +17,58 @@ public class s12 {
 //    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 
+//    public static boolean exist(char[][] board, String word) {
+//        int m = board.length;
+//        int n = board[0].length;
+//        char start = word.charAt(0);
+//        for(int i=0; i<m; i++){
+//            for(int j=0; j<n; j++){
+//                if(board[i][j]==start){
+//                    if(dfs(board, word, i, j, 0)){
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+//        return false;
+//
+//    }
+//    public static boolean dfs(char[][] board, String word, int i, int j, int k){
+//        int m = board.length;
+//        int n = board[0].length;
+//        if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word.charAt(k)){
+//            return false;
+//        }
+//        if(k==word.length()-1){
+//            return true;
+//        }
+//        board[i][j] = '\0';
+//        boolean res = dfs(board, word, i+1, j, k+1)
+//                || dfs(board, word, i-1, j, k+1)
+//                || dfs(board, word, i, j+1, k+1)
+//                || dfs(board, word, i, j-1, k+1);
+//        board[i][j] = word.charAt(k);
+//        return res;
+//
+//    }
+
     public static boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-        char start = word.charAt(0);
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(board[i][j]==start){
-                    if(dfs(board, word, i, j, 0)){
-                        return true;
-                    }
-                }
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, words, i, j, 0)) return true;
             }
         }
         return false;
-
     }
-    public static boolean dfs(char[][] board, String word, int i, int j, int k){
-        int m = board.length;
-        int n = board[0].length;
-        if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word.charAt(k)){
-            return false;
-        }
-        if(k==word.length()-1){
-            return true;
-        }
+    public static boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.length - 1) return true;
         board[i][j] = '\0';
-        boolean res = dfs(board, word, i+1, j, k+1)
-                || dfs(board, word, i-1, j, k+1)
-                || dfs(board, word, i, j+1, k+1)
-                || dfs(board, word, i, j-1, k+1);
-        board[i][j] = word.charAt(k);
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = word[k];
         return res;
-
     }
 
 }
