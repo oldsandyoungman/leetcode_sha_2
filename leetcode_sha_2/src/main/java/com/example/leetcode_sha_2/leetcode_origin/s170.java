79d87fb746f1a79512706251c6254eb54748d772
oldsandyoungman <874997364@qq.com>
2022-01-27 10:01:26 +0800

    twoSum数据结构题

---
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s170.java
@@ -0,0 +1,92 @@
+package com.example.leetcode_sha_2.leetcode_origin;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class s170 {
+
+    public static void main(String[] args) {
+
+    }
+
+//    设计一个数据结构，可以执行：
+//    1、添加一个数；
+//    2、给定一个数n nn，判断已经添加的数里是否有两个数的和恰好是n nn。已经添加的数每个数只能用一次。
+
+
+
+}
+
+
+// 方法一：加的复杂度为 O(1)，查的复杂度为 O(N)
+//class TwoSum {
+//
+//    HashMap<Integer, Integer> m;
+//
+//    /**
+//     * Initialize your data structure here.
+//     */
+//    public TwoSum() {
+//        m = new HashMap<>();
+//    }
+//
+//    /**
+//     * Add the number to an internal data structure..
+//     */
+//    public void add(int number) {
+//        m.put(number, m.getOrDefault(number, 0)+1);
+//    }
+//
+//    /**
+//     * Find if there exists any pair of numbers which sum is equal to the value.
+//     */
+//    public boolean find(int value) {
+//        for (Integer num : m.keySet()) {
+//            int other = value - num;
+//            if (other==num && m.get(num)>1) {
+//                return true;
+//            }
+//            if (other!=num && m.containsKey(other)){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
+
+
+// 方法一：加的复杂度为 O(N)，查的复杂度为 O(1)
+class TwoSum {
+
+    HashSet<Integer> sum;
+    HashSet<Integer> nums;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public TwoSum() {
+        sum = new HashSet<>();
+        nums = new HashSet<>();
+    }
+
+    /**
+     * Add the number to an internal data structure..
+     */
+    public void add(int number) {
+        for (Integer num : nums) {
+            int target = num + number;
+            sum.add(target);
+        }
+        nums.add(number);
+    }
+
+    /**
+     * Find if there exists any pair of numbers which sum is equal to the value.
+     */
+    public boolean find(int value) {
+        return sum.contains(value);
+    }
+
+}
+
+
