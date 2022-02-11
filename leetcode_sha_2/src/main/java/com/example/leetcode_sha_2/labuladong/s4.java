aef5e8b86b1a0d274d96180bb825a3b90217afd7
oldsandyoungman <874997364@qq.com>
2022-02-11 12:12:51 +0800

    水塘抽样法，重做了一遍

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/labuladong/s4.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/labuladong/s4.java
@@ -14,35 +14,78 @@ public class s4 {
 //    https://labuladong.gitee.io/algo/4/30/124/
 
 
+//    /* 返回链表中一个随机节点的值 */
+//    public static int getRandom(ListNode head) {
+//        Random r = new Random();
+//        int res = -1;
+//        int count = 1;
+//        ListNode p = head;
+//        while(p!=null){
+//            int tmp = r.nextInt(count);
+//            if (tmp==0) {
+//                res = p.val;
+//            }
+//            count++;
+//            p = p.next;
+//        }
+//        return res;
+//    }
+//
+//    /* 返回链表中 k 个随机节点的值 */
+//    public static int[] getRandom(ListNode head, int k) {
+//        Random r = new Random();
+//        int[] res = new int[k];
+//        ListNode p = head;
+//        for (int i=0; i<k; i++){
+//            res[i] = p.val;
+//            p = p.next;
+//        }
+//
+//        int i = k;
+//        while (p!=null) {
+//            i++;
+//            int tmp = r.nextInt(i);
+//            if(tmp<k){
+//                res[tmp] = p.val;
+//            }
+//            p = p.next;
+//        }
+//
+//        return res;
+//
+//    }
+
     /* 返回链表中一个随机节点的值 */
     public static int getRandom(ListNode head) {
-        Random r = new Random();
-        int res = -1;
-        int count = 1;
         ListNode p = head;
+        int sum = 0;
+        int res = -1;
+        Random r = new Random();
         while(p!=null){
-            int tmp = r.nextInt(count);
+            sum++;
+            int tmp = r.nextInt(sum);
             if (tmp==0) {
                 res = p.val;
             }
-            count++;
             p = p.next;
         }
+
         return res;
+
     }
 
     /* 返回链表中 k 个随机节点的值 */
     public static int[] getRandom(ListNode head, int k) {
-        Random r = new Random();
         int[] res = new int[k];
         ListNode p = head;
-        for (int i=0; i<k; i++){
+        Random r = new Random();
+        int i = 0;
+        for(; i<k; i++){
             res[i] = p.val;
             p = p.next;
         }
 
-        int i = k;
-        while (p!=null) {
+        while(p!=null){
             i++;
             int tmp = r.nextInt(i);
             if(tmp<k){
