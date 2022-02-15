72d4f0cd3928c662001084908dcf443c864df297
oldsandyoungman <874997364@qq.com>
2022-02-15 08:51:46 +0800

    安排会议室问题，类似差分数组的思路

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s494.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s494.java
@@ -5,6 +5,8 @@ public class s494 {
     public static void main(String[] args) {
         int[] nums = {0,0,0,0,0,0,0,0,1};
         int target = 1;
+//        int[] nums = {1,1,1,1,1};
+//        int target = 3;
         System.out.println(findTargetSumWays(nums, target));
 
     }
@@ -97,18 +99,41 @@ public class s494 {
 
     }
     public static int bag(int[] nums, int target){
+//        int n = nums.length;
+//        int[][] dp = new int[n+1][target+1];
+//        // for(int i=0; i<=target; i++){
+//        //     dp[0][i] = 0;
+//        // }
+//        for(int i=0; i<=n; i++){
+//            dp[i][0] = 1;
+//        }
+//
+//        for(int i=1; i<=n; i++){
+//            for(int j=0; j<=target; j++){
+//                if(j>=nums[i-1]){
+//                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+//                }else{
+//                    dp[i][j] = dp[i-1][j];
+//                }
+//            }
+//        }
+//
+//        return dp[n][target];
+
+
         int n = nums.length;
+        // dp[i][j]: nums[0]-nums[i-1]中任选物品，使得总和为j的方案数量
         int[][] dp = new int[n+1][target+1];
-        // for(int i=0; i<=target; i++){
-        //     dp[0][i] = 0;
-        // }
+        // base
+        // dp[i][0] = 1 （dp[i][0]只是初始化为1，因为nums中可能有0，所以之后可能会继续变大）
+        // dp[0][j] = 0;
         for(int i=0; i<=n; i++){
             dp[i][0] = 1;
         }
 
         for(int i=1; i<=n; i++){
             for(int j=0; j<=target; j++){
-                if(j>=nums[i-1]){
+                if(nums[i-1]<=j){
                     dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
                 }else{
                     dp[i][j] = dp[i-1][j];
@@ -116,7 +141,10 @@ public class s494 {
             }
         }
 
+//        System.out.println(Arrays.deepToString(dp));
+
         return dp[n][target];
+
 
     }
 
