e6667a36f03f4d3aad29e1cdce1264292575a71c
oldsandyoungman <874997364@qq.com>
2022-02-17 09:11:43 +0800

    二叉树的迭代写法

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/labuladong/s1.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/labuladong/s1.java
@@ -3,6 +3,7 @@ package com.example.leetcode_sha_2.labuladong;
 import com.example.leetcode_sha_2.class_sha.TreeNode;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -63,44 +64,97 @@ public class s1 {
 
 
 
-    public static ArrayDeque<TreeNode> q = new ArrayDeque<>();
+//    public static ArrayDeque<TreeNode> q = new ArrayDeque<>();
+//
+//    public static void pushLeftBranch(TreeNode p){
+//        while(p!=null){
+//            // 前序
+//            q.addLast(p);
+//            p = p.left;
+//        }
+//    }
+//
+//    public static List<Integer> traverse(TreeNode root){
+//
+//        List<Integer> res = new LinkedList<>();
+//
+//        TreeNode visited = new TreeNode(-1);
+//
+//        pushLeftBranch(root);
+//
+//        while (!q.isEmpty()) {
+//
+//            TreeNode p = q.getLast();
+//
+//            if ((p.left==null || p.left==visited) && p.right!=visited) {
+//                // 中序
+//                pushLeftBranch(p.right);
+//            }
+//
+//            if (p.right==null || p.right==visited) {
+//                //后续
+//                res.add(p.val);
+//                visited = q.removeLast();
+//            }
+//
+//        }
+//
+//        return res;
+//
+//    }
 
-    public static void pushLeftBranch(TreeNode p){
-        while(p!=null){
+
+
+
+
+    public static List<Integer> res;
+
+    public static ArrayDeque<TreeNode> q;
+
+    public static void pushLeft(TreeNode root){
+        while(root!=null){
+            q.addLast(root);
+
             // 前序
-            q.addLast(p);
-            p = p.left;
+
+
+            root = root.left;
         }
     }
 
     public static List<Integer> traverse(TreeNode root){
+        res = new ArrayList<>();
+        q = new ArrayDeque<>();
 
-        List<Integer> res = new LinkedList<>();
+        pushLeft(root);
 
         TreeNode visited = new TreeNode(-1);
 
-        pushLeftBranch(root);
+        while(!q.isEmpty()){
+            TreeNode cur = q.getLast();
 
-        while (!q.isEmpty()) {
+            if ((cur.left==null || cur.left==visited) && cur.right!=visited) {
 
-            TreeNode p = q.getLast();
+                // 中序遍历
 
-            if ((p.left==null || p.left==visited) && p.right!=visited) {
-                // 中序
-                pushLeftBranch(p.right);
+                pushLeft(cur.right);
+
             }
 
-            if (p.right==null || p.right==visited) {
-                //后续
-                res.add(p.val);
+            if (cur.right==null || cur.right==visited) {
+                res.add(cur.val);
                 visited = q.removeLast();
+
             }
+
+
 
         }
 
         return res;
 
     }
+
 
 
 
