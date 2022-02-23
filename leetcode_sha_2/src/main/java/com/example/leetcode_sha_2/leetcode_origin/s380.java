37cda42ab4307e8132353cb7ec86b9ad48703c7e
oldsandyoungman <874997364@qq.com>
2022-02-23 08:58:07 +0800

    O(1)时间插入删除获取元素

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s380.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/leetcode_origin/s380.java
@@ -7,53 +7,67 @@ import java.util.Random;
 public class s380 {
 
     public static void main(String[] args) {
-        ArrayList<Integer> l = new ArrayList<>();
-        Random rd = new Random();
-        rd.nextInt();
+//        ArrayList<Integer> l = new ArrayList<>();
+//        Random rd = new Random();
+//        rd.nextInt();
+
+
+        RandomizedSet r = new RandomizedSet();
+        System.out.println(r.remove(0));
+        System.out.println(r.remove(0));
+        System.out.println(r.insert(0));
+
+        System.out.println(r.getRandom());
+
+        System.out.println(r.remove(0));
+        System.out.println(r.insert(0));
+
+
+
     }
 
 }
 
-class RandomizedSet {
-    // val -> index
-    HashMap<Integer, Integer> m;
-    ArrayList<Integer> nums;
-    Random rd;
-
-    public RandomizedSet() {
-        m = new HashMap<>();
-        nums = new ArrayList<>();
-        rd = new Random();
-    }
-
-    public boolean insert(int val) {
-        if(m.containsKey(val)){
-            return false;
-        }
-
-        m.put(val, nums.size());
-        nums.add(val);
-        return true;
-    }
-
-    public boolean remove(int val) {
-        if(!m.containsKey(val)){
-            return false;
-        }
-
-        int index = m.get(val);
-        int lastVal = nums.get(nums.size()-1);
-        m.put(lastVal, index);
-        nums.set(index, lastVal);
-        nums.remove(nums.size()-1);
-        m.remove(val);
-        return true;
-    }
-
-    public int getRandom() {
-        return nums.get(rd.nextInt(nums.size()));
-    }
-}
+//class RandomizedSet {
+//    // val -> index
+//    HashMap<Integer, Integer> m;
+//    ArrayList<Integer> nums;
+//    Random rd;
+//
+//    public RandomizedSet() {
+//        m = new HashMap<>();
+//        nums = new ArrayList<>();
+//        rd = new Random();
+//    }
+//
+//    public boolean insert(int val) {
+//        if(m.containsKey(val)){
+//            return false;
+//        }
+//
+//        m.put(val, nums.size());
+//        nums.add(val);
+//        return true;
+//    }
+//
+//    public boolean remove(int val) {
+//        if(!m.containsKey(val)){
+//            return false;
+//        }
+//
+//        int index = m.get(val);
+//        int lastVal = nums.get(nums.size()-1);
+//        m.put(lastVal, index);
+//        nums.set(index, lastVal);
+//        nums.remove(nums.size()-1);
+//        m.remove(val);
+//        return true;
+//    }
+//
+//    public int getRandom() {
+//        return nums.get(rd.nextInt(nums.size()));
+//    }
+//}
 
 /**
  * Your RandomizedSet object will be instantiated and called as such:
@@ -62,3 +76,48 @@ class RandomizedSet {
  * boolean param_2 = obj.remove(val);
  * int param_3 = obj.getRandom();
  */
+
+
+
+class RandomizedSet {
+
+    HashMap<Integer, Integer> valToIndex;
+    int count;
+    int[] nums;
+    Random r;
+
+    public RandomizedSet() {
+        valToIndex = new HashMap<>();
+        count = 0;
+        nums = new int[200000];
+        r = new Random();
+    }
+
+    public boolean insert(int val) {
+        if(valToIndex.containsKey(val)){
+            return false;
+        }
+        nums[count] = val;
+        valToIndex.put(val, count);
+        count++;
+        return true;
+    }
+
+    public boolean remove(int val) {
+        if(!valToIndex.containsKey(val)){
+            return false;
+        }
+        int index = valToIndex.get(val);
+        int tail_num = nums[count-1];
+        nums[index] = tail_num;
+        valToIndex.remove(val);
+        valToIndex.put(tail_num, index);
+        count--;
+        return true;
+    }
+
+    public int getRandom() {
+        int index = r.nextInt(count);
+        return nums[index];
+    }
+}
