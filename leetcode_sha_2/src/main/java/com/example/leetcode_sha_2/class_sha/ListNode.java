e6af5688fe7e8e67cee00832b0efaf066abbcabb
oldsandyoungman <874997364@qq.com>
2022-03-26 10:00:11 +0800

    数组创建链表函数

--- leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/class_sha/ListNode.java
+++ leetcode_sha_2/src/main/java/com/example/leetcode_sha_2/class_sha/ListNode.java
@@ -1,5 +1,7 @@
 package com.example.leetcode_sha_2.class_sha;
 
+import java.util.List;
+
 public class ListNode {
     public int val;
     public ListNode next;
@@ -14,6 +16,22 @@ public class ListNode {
                     "->" +
                     next;
         }
+    }
+
+    public static ListNode create_by_nums(int[] nums){
+        if(nums.length==0){
+            return null;
+        }
+        ListNode head = new ListNode(nums[0]);
+        ListNode cur = head;
+
+        for (int i = 1; i < nums.length; i++) {
+            cur.next = new ListNode(nums[i]);
+            cur = cur.next;
+        }
+
+        return head;
 
     }
+
 }
